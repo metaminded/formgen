5e85fd8bb8b8dfa94fed29df8da8b61d77a7c0f7
Philipp Hirsch <itself@hanspolo.net>
2014-06-11 12:40:51 +0200

    Added show for replies

--- app/controllers/formgen/answers_controller.rb
+++ app/controllers/formgen/answers_controller.rb
@@ -4,14 +4,13 @@ module Formgen
   class AnswersController < ApplicationController
     include AnswersHelper
 
-    def show
-    end
+    before_action :get_reply, only: [:show]
 
     def create
       errors = []
       @form = Form.find params[:id]
       ActiveRecord::Base.transaction do
-        @reply = Reply.create! form: @form, user: current_user.present? ? current_user : nil
+        @reply = Reply.create! form: @form, user: current_user.present? && current_user.class == "User" ? current_user : nil
         @form.questions.each do |question|
           missing_field_error(errors, question) if question.mandatory && params[:reply][question.id.to_s].empty?
           Answer.create! reply: @reply, question: question, value: params[:reply][question.id.to_s]
@@ -24,8 +23,14 @@ module Formgen
       else
         flash[:notice] = t('.saved_reply_successfully')
       end
-      
+
       redirect_to :back
+    end
+
+    private
+
+    def get_reply
+      @reply = Reply.includes(answers: :question).find(params[:id])
     end
   end
 end
