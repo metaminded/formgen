c3ba4ca496e001c65ce5dbb40318cd1b6b6078a6
Philipp Hirsch <itself@hanspolo.net>
2014-06-11 10:59:21 +0200

    Added helpers

--- app/helpers/formgen/application_helper.rb
+++ app/helpers/formgen/application_helper.rb
@@ -1,4 +1,12 @@
 module Formgen
   module ApplicationHelper
+    include FontAwesome::Rails::IconHelper
+
+    def render_form path: '', options: {}
+      path = request.path if path.empty?
+      form = Form.find_or_create_by path: path
+
+      render 'formgen/forms/output', form: form, options: options
+    end
   end
 end
--- lib/formgen/engine.rb
+++ lib/formgen/engine.rb
@@ -1,5 +1,12 @@
 module Formgen
   class Engine < ::Rails::Engine
     isolate_namespace Formgen
+
+
+    initializer 'formgen.action_controller' do |app|
+      ActiveSupport.on_load :action_controller do
+        helper Formgen::ApplicationHelper
+      end
+    end
   end
 end
