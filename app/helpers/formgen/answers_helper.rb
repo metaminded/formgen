6d425143df75a124809f6d407b3ec9a5c49d3abc
Philipp Hirsch <itself@hanspolo.net>
2014-06-16 13:53:44 +0200

    Added output of answers to helpers

--- app/helpers/formgen/answers_helper.rb
+++ app/helpers/formgen/answers_helper.rb
@@ -10,5 +10,18 @@ module Formgen
       output << "</ul>"
       output
     end
+
+    def save_reply
+      errors = []
+      ActiveRecord::Base.transaction do
+        @reply = Reply.create! form: @form, user: current_user.present? && current_user.class.to_s == "User" ? current_user : nil
+        @form.questions.each do |question|
+          missing_field_error(errors, question) if question.mandatory && params[:reply][question.id.to_s].empty?
+          Answer.create! reply: @reply, question: question, value: params[:reply][question.id.to_s]
+        end
+        raise ActiveRecord::Rollback unless errors.empty?
+      end
+      errors
+    end
   end
 end
--- app/helpers/formgen/application_helper.rb
+++ app/helpers/formgen/application_helper.rb
@@ -1,6 +1,7 @@
 module Formgen
   module ApplicationHelper
     include FontAwesome::Rails::IconHelper
+    include AnswersHelper
 
     def render_form path: '', options: {}
       path = request.path if path.empty?
@@ -31,12 +32,21 @@ module Formgen
       render 'formgen/forms/questions', f: form_for, options: options
     end
 
-    def render_reply_into_other_form path: '', options: {}
+    def render_reply_for form_for, obj, path: '', options: {}
+      path = request.path if path.empty?
+      form = Form.find_or_create_by path: path
+      obj.form = form
 
+      render 'formgen/answers/answers', f: form_for, form: form, options: options
     end
 
-    def save_reply_out_of_other_form path: '', params: {}
+    def save_reply_for path: ''
+      path = request.path if path.empty?
+      @form = Form.find_by path: path
 
+      errors = save_reply
+
+      NotificationMailer.notify(@reply).deliver
     end
   end
 end
