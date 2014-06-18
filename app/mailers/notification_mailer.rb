b991209a920c08f0e8b3cd964019e902cd24521b
Philipp Hirsch <itself@hanspolo.net>
2014-06-18 11:19:07 +0200

    Better i18n of notification mails

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,9 +2,9 @@ class NotificationMailer < ActionMailer::Base
   def notify(reply)
     @reply = reply
     email = reply.form.email
-    title = reply.form.title
+    title = reply.form.title.presence || reply.form.path
 
-    mail(from: '', to: email, subject: t('.new_notification_for', title: title), template_name: 'notify')
+    mail(from: 'me', to: email, subject: t('notification_mailer.notify.new_notification_for', title: title), template_name: 'notify')
   end
 
   def send_mail(method, *args)
