69b03c2178db9c7e7de0a48b5319f403b772ad32
Philipp Hirsch <itself@hanspolo.net>
2014-07-15 18:32:09 +0200

    Using new button helpers from bsat & hide submit button for empty forms

--- app/tabulatr_data/formgen/form_tabulatr_data.rb
+++ app/tabulatr_data/formgen/form_tabulatr_data.rb
@@ -1,12 +1,11 @@
 module Formgen
   #
   class FormTabulatrData < Tabulatr::Data
-    column :actions, table_column_options: { header: '', sortable: false, filter: false } do
-      [
-        link_to(fa_icon('edit'), formgen.edit_form_path(record.id)),
-        link_to(fa_icon('eye'), formgen.form_path(record.id))
-      ]
+    buttons do |b, r|
+      b.button :eye,  formgen.form_path(r.id)
+      b.button :pencil, formgen.edit_form_path(r.id)
     end
+
     column :title
     column :path
   end
