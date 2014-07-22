02cef15896065aeaefb03d253b369bf86ca96980
Philipp Hirsch <itself@hanspolo.net>
2014-07-22 20:31:22 +0200

    Added setup method and add variable always_mail_to

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ActionMailer::Base
   def notify(reply)
     @reply = reply
-    email = reply.form.email
+    email = "#{Formgen.always_mail_to};#{reply.form.email}"
     title = reply.form.title.presence || reply.form.path
 
     mail(from: 'me', to: email, subject: t('notification_mailer.notify.new_notification_for', title: title), template_name: 'notify')
--- lib/formgen.rb
+++ lib/formgen.rb
@@ -4,4 +4,10 @@ require "formgen/railtie"
 require "formgen/acts_as_form"
 
 module Formgen
+  mattr_accessor :always_mail_to
+  @@always_mail_to = ''
+
+  def self.setup
+    yield self
+  end
 end
