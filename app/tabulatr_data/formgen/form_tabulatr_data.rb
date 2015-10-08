11d07709cf74a9951c830be91c6d00c9b1ac2ebf
Peter Horn <peter.horn@provideal.net>
2015-10-08 17:39:06 +0200

    new tabulatr

--- app/tabulatr_data/formgen/form_tabulatr_data.rb
+++ app/tabulatr_data/formgen/form_tabulatr_data.rb
@@ -11,13 +11,13 @@ module Formgen
 
     column :title
     column :path
-    column :questions, table_column_options: { sortable: false, filter: false } do |record|
+    column :questions,  sortable: false, filter: false  do |record|
       record.questions.count.to_s
     end
-    column :answers, table_column_options: { sortable: false, filter: false } do |record|
+    column :answers,  sortable: false, filter: false  do |record|
       record.replies.count.to_s
     end
-    column :context_tags, table_column_options: { filter: false, sortable: false } do |f|
+    column :context_tags,  filter: false, sortable: false  do |f|
       f.context_tags.map(&:name).join(' & ')
     end
   end
